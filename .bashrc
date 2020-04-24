3fb1765b3a9f398d4aed80e8dc3ad10325b2cb03
Alex Standke <astandke@vmware.com>
2020-04-24 00:34:44 +0000

    fix co-authored-by
    Co-authored-by: Tim Downey <tdowney@vmware.com>

--- .bashrc
+++ .bashrc
@@ -88,6 +88,7 @@ fi
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export GIT_DUET_CO_AUTHORED_BY=1
 export GIT_DUET_GLOBAL=1
+export GIT_DUET_ROTATE_AUTHOR=1
 export EDITOR=nvim
 
 # some more ls aliases
--- gitconfig.sh
+++ gitconfig.sh
@@ -6,4 +6,6 @@ ln -sf ${HOME}/workspace/networking-workspace/git-authors ${HOME}/.git-authors
 echo "updating all git repos to use 'git co-author'"
 git solo as # HACK: if not set to anything git duet fails in the following commands
 export GIT_DUET_CO_AUTHORED_BY=1
+export GIT_DUET_GLOBAL=1
+export GIT_DUET_ROTATE_AUTHOR=1
 find ~/workspace/ -type d -name '.git' -exec sh -c 'cd {} && cd .. && git init' \;
