80655f758915fee4ce3e9d888f80324f610f7832
Alex Standke <astandke@vmware.com>
2020-04-28 18:33:01 +0000

    use ssh urls globally

--- gitconfig.sh
+++ gitconfig.sh
@@ -12,3 +12,6 @@ export GIT_DUET_CO_AUTHORED_BY=1
 export GIT_DUET_GLOBAL=1
 export GIT_DUET_ROTATE_AUTHOR=1
 find ~/workspace/ -type d -name '.git' -exec sh -c 'cd {} && cd .. && git init' \;
+
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+
