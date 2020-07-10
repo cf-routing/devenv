cb415b9b8b3bf2211c3a1ed5338d52f710e9c187
Alex Standke <astandke@vmware.com>
2020-07-10 18:28:26 +0000

    delete old devenv image before trying to capture a new one

--- pipeline/tasks/capture_image.sh
+++ pipeline/tasks/capture_image.sh
@@ -10,8 +10,12 @@ gcloud auth activate-service-account \
   --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") \
   --project="cf-routing" 1>/dev/null 2>&1
 
+
 echo "Stopping Canary..."
 gcloud compute instances stop canary --zone us-central1-a
+
+echo "Deleting the old image"
+gcloud compute images create devenv --quiet
 
 echo "Capturing image..."
 # https://cloud.google.com/compute/docs/images/create-delete-deprecate-private-images#gcloud
