024e28f7d8381c998a49b8dddb8fc5b5ca2ce7b2
Jen Spinney <jspinney@vmware.com>
2020-07-13 15:05:32 -0700

    This adds the ability to use the arrow keys to navigate bash history
    In particular, this adds the ability to use 'up arrow' when a command is
    partially written to cycle through all commands in bash history that
    start with the given prefix.

--- configure.sh
+++ configure.sh
@@ -19,6 +19,7 @@ wget -O - https://raw.githubusercontent.com/luan/tmuxfiles/master/install | bash
 # install bash profile
 rm ~/.bash_aliases ~/.bash_logout ~/.bash_profile ~/.bashrc ~/.profile
 ln -s $(pwd)/.bash* ~/
+ln -s $(pwd)/.inputrc ~/
 ln -s $(pwd)/.profile ~/
 
 # fly aliases
