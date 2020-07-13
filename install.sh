cc321e097849d57b7ea13b11c0f3610b24234745
Alex Standke <astandke@vmware.com>
2020-07-13 23:21:28 +0000

    install kind

--- install.sh
+++ install.sh
@@ -157,3 +157,8 @@ pushd /tmp/istio
   curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.4 sh -
   sudo mv istio-*/bin/istioctl /usr/local/bin
 popd
+
+# kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
