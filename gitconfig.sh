4cbc14a94e13e5bd93e109d825cc03be98c6815b
Alex Standke <astandke@vmware.com>
2020-07-30 19:03:16 +0000

    attempt to fix co-authored-by missing issue
    Co-authored-by: Kauana dos Santos <kdossantos@vmware.com>

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,7 +7,7 @@ echo "Symlink the git-authors file to .git-authors..."
 ln -sf ${HOME}/workspace/networking-workspace/git-authors ${HOME}/.git-authors
 
 echo "updating all git repos to use 'git co-author'"
-git solo as # HACK: if not set to anything git duet fails in the following commands
+git duet as kds # HACK: if not set to anything git duet fails in the following commands
 export GIT_DUET_CO_AUTHORED_BY=1
 export GIT_DUET_GLOBAL=1
 export GIT_DUET_ROTATE_AUTHOR=1
