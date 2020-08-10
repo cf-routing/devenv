4576233422adf52c01975800e3b60905fefef292
Mikael Manukyan <mmanukyan@vmware.com>
2020-08-10 18:06:28 +0000

    Run fzf install as pivotal user
    Co-authored-by: Kauana dos Santos <kdossantos@vmware.com>

--- install.sh
+++ install.sh
@@ -157,8 +157,10 @@ pip3 install yq neovim when-changed
 curl -L https://k14s.io/install.sh | bash
 
 # fzf
+su - pivotal
 git clone --depth 1 https://github.com/junegunn/fzf.git /home/pivotal/.fzf
-sudo ~/.fzf/install --key-bindings --completion --no-update-rc
+sudo /home/pivotal/.fzf/install --key-bindings --completion --no-update-rc
+exit
 
 # istioctl
 mkdir /tmp/istio
