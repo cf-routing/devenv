2d7d8d30b0541e7276f6cd43cc54a26e799a298c
Alex Standke <astandke@vmware.com>
2020-09-15 22:37:22 +0000

    remove need for :GoInstallBinaries on fresh machine

--- install-as-pivotal.sh
+++ install-as-pivotal.sh
@@ -34,3 +34,23 @@ export PATH=$PATH:/usr/local/go/bin:/home/xander/.local/bin:$HOME/go/bin/
 go get -u github.com/dbellotti/cf-target
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega/...
+
+# vim-go dependencies so you don't have to run :GoInstallBinaries
+#   source: https://github.com/fatih/vim-go/blob/master/plugin/go.vim#L42
+go get github.com/klauspost/asmfmt/cmd/asmfmt
+go get github.com/go-delve/delve/cmd/dlv
+go get github.com/kisielk/errcheck
+go get github.com/davidrjenni/reftools/cmd/fillstruct
+go get github.com/rogpeppe/godef
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/lint/golint
+go get golang.org/x/tools/gopls
+go get github.com/golangci/golangci-lint/cmd/golangci-lint
+go get github.com/fatih/gomodifytags
+go get golang.org/x/tools/cmd/gorename
+go get github.com/jstemmer/gotags
+go get golang.org/x/tools/cmd/guru
+go get github.com/josharian/impl
+go get honnef.co/go/tools/cmd/keyify
+go get github.com/fatih/motion
+go get github.com/koron/iferr
