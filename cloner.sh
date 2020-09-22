88c2ae3724691400d79e3837300b815f305f80e2
David McClure <dmcclure@vmware.com>
2020-09-22 19:20:49 +0000

    Add smith CLI to cloner.sh
    Include docs for installing GlobalProtect VPN

--- cloner.sh
+++ cloner.sh
@@ -125,6 +125,22 @@ clone_if_not_exist "git@github.com:pivotal-cf/docs-partials" "${HOME}/workspace/
 clone_if_not_exist "git@github.com:pivotal-cf/docs-pcf-security" "${HOME}/workspace/docs-pcf-security"
 clone_if_not_exist "git@github.com:cloudfoundry/docs-running-cf" "${HOME}/workspace/docs-running-cf"
 
+# smith CLI for interacting with toolsmiths envs
+clone_if_not_exist "git@github.com:pivotal/smith" "${HOME}/workspace/smith"
+# Note: requires VPN access
+# Install GlobalProtect (not automated because we can't distribute file ourselves)
+# 1. Download https://drive.google.com/a/pivotal.io/file/d/1GxaJGgvoTapDjdq1J3qCkxRBAztIbtOQ/view?usp=sharing
+# 2. tar -xzvf PanGPLinux-5.1.0-c101.tgz
+# 3. gcloud beta compute scp --zone us-central1-a --project "cf-routing" GlobalProtect_deb-5.1.0.0-101.deb pivotal@${workstation}:~
+# 4. sudo dpkg -i GlobalProtect_deb-5.1.0.0-101.deb
+#
+# Connect to VPN
+# 1. globalprotect connect --portal portal-nasa.vpn.pivotal.io -u ${username}
+# 2. Choose 1 (Okta verify)
+#
+# Install smith
+# 1. cd ~/workspace/smith && go install
+
 cd ~/workspace
 
 echo "direnv allow all releases for all repos"
