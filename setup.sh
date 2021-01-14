141dc99681ed16315e4d941af57c18cb96dce8ed
Josh Russett <jrussett@vmware.com>
2021-01-14 23:48:29 +0000

    wip: modify setup to accept existing repo

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-mkdir -p workspace
-cd workspace
-echo "Cloning the devenv scripts"
-git clone https://github.com/cf-routing/devenv.git
+workspaceDir="${HOME}/workspace"
+devenvDir="${workspaceDir}/devenv"
+
+if [[ ! -d "${devenvDir}" ]]; then
+    mkdir -p "${workspaceDir}"
+    printf "devenv not found.\nCloning the devenv scripts\n"
+    git clone https://github.com/cf-routing/devenv.git "${devenvDir}"
+fi
 
 cd devenv
 echo "Cloning our team project repos"
-./cloner.sh
+"${devenvDir}/cloner.sh"
+
 echo "Installing required packages and tools, you may be prompted for your sudo password."
-sudo ./install.sh
-./configure.sh
-./gitconfig.sh
-cd ~
+sudo "${devenvDir}/install.sh"
+"${devenvDir}/configure.sh"
+"${devenvDir}/gitconfig.sh"
+cd
 
 echo "All done! Disconnect and log back in to ensure you have everything."
-echo "Some repositories can't be cloned without a private key, install your identity (with pivotal_login or similar) and run ./workspace/devenv/cloner.sh to get the rest."
+echo "Some repositories can't be cloned without a private key, install your identity (with pivotal_login or similar) and run ${devenvDir}/cloner.sh to get the rest."
