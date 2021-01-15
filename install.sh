fb817843f97aaac03857b06df851ec9f8d64ad0d
Josh Russett <jrussett@vmware.com>
2021-01-15 23:03:15 +0000

    wip: update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,17 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
-workspace_path=/home/pivotal/workspace
-
 apt-get update
-apt-get install apt-utils
+apt-get install -yq apt-utils
+
+# Add apt PPAs
+# add cf-clis
+curl -L "https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key" | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+
+# add neovim
+add-apt-repository ppa:neovim-ppa/stable
+
 apt-get update
 apt-get install -yq \
   apt-transport-https \
@@ -20,9 +27,11 @@ apt-get install -yq \
   autoconf \
   automake \
   awscli \
+  azure-cli \
   bash-completion \
   bison \
   build-essential \
+  cf7-cli \
   cowsay \
   curl \
   direnv \
@@ -40,92 +49,72 @@ apt-get install -yq \
   libssl-dev \
   libyaml-dev \
   lsb-release \
+  neovim \
   npm \
   openconnect \
   python3-pip \
   ripgrep \
+  shellcheck \
   silversearcher-ag \
   software-properties-common \
-  shellcheck \
   tmux \
   tree \
   wget \
   xclip \
-  zlib1g-dev \
+  zlib1g-dev
 
-sudo ln -s "$(which fdfind)" /usr/bin/fd
+ln -s "$(command -v fdfind)" /usr/bin/fd
 
-# add cf-clis
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-
-apt-get update
-
-# install the rest
-apt-get install -yq azure-cli cf7-cli
-
-curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
-
-# install eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+curl -o "/usr/share/bash-completion/completions/cf" "https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf"
 
 # install golang the right way
 mkdir -p /tmp/installscratch
-cd /tmp/installscratch
-wget "https://dl.google.com/go/$(curl https://golang.org/VERSION?m=text).linux-amd64.tar.gz"
-tar -xvf go*
-rm -rf /usr/local/go
-mv go /usr/local
-rm -rf /tmp/installscratch
+pushd /tmp/installscratch
+  curl -L "https://dl.google.com/go/$(curl https://golang.org/VERSION?m=text).linux-amd64.tar.gz" | tar -zx
+  rm -rf /usr/local/go
+  mv go /usr/local
+popd
 
 # install user specific programs as pivotal
-sudo -u pivotal /home/pivotal/workspace/devenv/install-as-pivotal.sh
-
-# neovim
-sudo snap install nvim --classic
+sudo -u pivotal "/home/pivotal/workspace/devenv/install-as-pivotal.sh"
 
 # git-duet
 mkdir -p /tmp/git-duet
-cd
 pushd /tmp/git-duet
-  url=$(curl -s https://api.github.com/repos/git-duet/git-duet/releases | jq -r '.[0].assets[] | select(.name=="linux_amd64.tar.gz").browser_download_url')
-  wget $url
-  tar -xvf linux_amd64.tar.gz
-  mv * /usr/local/bin/
+  url="$(curl -s https://api.github.com/repos/git-duet/git-duet/releases | jq -r '.[0].assets[] | select(.name=="linux_amd64.tar.gz").browser_download_url')"
+  curl -L "${url}" | tar -zx
+  install ./* /usr/local/bin/
 popd
 
 # git-duet
 mkdir -p /tmp/sshb0t
 pushd /tmp/sshb0t
-  url=$(curl -s https://api.github.com/repos/genuinetools/sshb0t/releases | jq -r '.[0].assets[] | select(.name=="sshb0t-linux-amd64").browser_download_url')
-  wget -O sshb0t $url
-  chmod +x sshb0t
-  mv sshb0t /usr/local/bin/
+  url="$(curl -s https://api.github.com/repos/genuinetools/sshb0t/releases | jq -r '.[0].assets[] | select(.name=="sshb0t-linux-amd64").browser_download_url')"
+  curl -L "${url}" -o sshb0t
+  install sshb0t /usr/local/bin/
 popd
-
 
 # bosh
 mkdir -p /tmp/bosh
 pushd /tmp/bosh
-url=$(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases | jq -r '.[0].assets[] | select(.name | contains("linux-amd64")).browser_download_url')
-  wget -O bosh "$url"
-  chmod +x bosh
-  mv bosh /usr/local/bin/
+url="$(curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases | jq -r '.[0].assets[] | select(.name | contains("linux-amd64")).browser_download_url')"
+  curl -L "${url}" -o bosh
+  install bosh /usr/local/bin/
 popd
 
 # bbl
-bbl_version="8.4.0"
-wget https://github.com/cloudfoundry/bosh-bootloader/releases/download/v${bbl_version}/bbl-v${bbl_version}_linux_x86-64 -P /tmp && \
-mv /tmp/bbl-* /usr/local/bin/bbl && \
-cd /usr/local/bin && \
-chmod +x bbl
+mkdir -p /tmp/bbl
+pushd /tmp/bbl
+  url=$(curl -s https://api.github.com/repos/cloudfoundry/bosh-bootloader/releases | jq -r '.[0].assets[] | select((.name | contains("linux"))).browser_download_url')
+  curl -L "${url}" -o bbl
+  install bbl /usr/local/bin/bbl
+popd
 
 #credhub cli
-credhub_cli_version="2.7.0"
-wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/${credhub_cli_version}/credhub-linux-${credhub_cli_version}.tgz -P /tmp && \
-tar xzvf /tmp/credhub-linux-${credhub_cli_version}.tgz -C /usr/local/bin && \
-chmod +x /usr/local/bin/credhub
+  url="$(curl -s https://api.github.com/repos/cloudfoundry-incubator/credhub-cli/releases | jq -r '.[0].assets[] | select((.name | contains("linux")) and (.name | contains("tgz"))).browser_download_url')"
+  curl -L "${url}" | tar -zx
+  install credhub /usr/local/bin/credhub
+popd
 
 # pip3 things
 pip3 install yq neovim when-changed
@@ -133,25 +122,25 @@ pip3 install yq neovim when-changed
 # kiln
 mkdir -p /tmp/kiln
 pushd /tmp/kiln
-  url=$(curl -s https://api.github.com/repos/pivotal-cf/kiln/releases | jq -r '.[0].assets[] | select((.name | contains("linux")) and (.name | contains("tar"))).browser_download_url')
+  url="$(curl -s https://api.github.com/repos/pivotal-cf/kiln/releases | jq -r '.[0].assets[] | select((.name | contains("linux")) and (.name | contains("tar"))).browser_download_url')"
   curl -L "${url}" | tar -zx
-  sudo install kiln /usr/local/bin/kiln
+  install kiln /usr/local/bin/kiln
 popd
 
 # om cli
 mkdir -p /tmp/om
 pushd /tmp/om
-  url=$(curl -s https://api.github.com/repos/pivotal-cf/om/releases | jq -r '.[0].assets[] | select((.name | contains("linux")) and (.name | contains("tar"))).browser_download_url')
+  url="$(curl -s https://api.github.com/repos/pivotal-cf/om/releases | jq -r '.[0].assets[] | select((.name | contains("linux")) and (.name | contains("tar"))).browser_download_url')"
   curl -L "${url}" | tar -zx
-  sudo install om /usr/local/bin/om
+  install om /usr/local/bin/om
 popd
 
 # certstrap
 mkdir -p /tmp/certstrap
 pushd /tmp/certstrap
   url=$(curl -s https://api.github.com/repos/square/certstrap/releases | jq -r '.[0].assets[] | select((.name | contains("linux"))).browser_download_url')
-  curl -L "${url}" --output certstrap
-  sudo install certstrap /usr/local/bin/certstrap
+  curl -L "${url}" -o certstrap
+  install certstrap /usr/local/bin/certstrap
 popd
 
 
